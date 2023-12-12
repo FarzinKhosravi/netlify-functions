7fa34a20c01e114d7da7d4e252fbfff61a945c9c
FarzinKhosravi <khosravii.farzin@gmail.com>
2023-12-12 15:03:09 +0330

    updated

--- functions/dataApi.js
+++ functions/dataApi.js
@@ -1,9 +1,15 @@
 import data from "./data.json";
 
+const headers = {
+  "content-type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+};
+
 export const handler = async () => {
   const response = {
     statusCode: 200,
     body: JSON.stringify(data),
+    headers: headers,
   };
 
   return response;
